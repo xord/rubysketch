5d4d0ea2ce5bd5bc4b8cf192825ea42c2f5aaa83
xordog <xordog@gmail.com>
2023-04-14 00:29:53 +0900

    add pause(), stop(), playing?(), paused?(), stopped?(), and save() to Sound

--- lib/rubysketch/sound.rb
+++ lib/rubysketch/sound.rb
@@ -9,11 +9,44 @@ module RubySketch
     end
 
     def play()
-      @sound&.play
+      old = @player
+      @player = @sound.play
+      old&.stop
+      true
+    end
+
+    def pause()
+      return false unless @player
+      @player.pause
+      true
+    end
+
+    def stop()
+      return false unless @player
+      @player.stop
+      @player = nil
+      true
+    end
+
+    def playing?()
+      @player ? @player.playing? : false
+    end
+
+    def paused?()
+      !playing? && !stopped?
+    end
+
+    def stopped?()
+      @player ? @player.stopped? : true
+    end
+
+    def save(path)
+      @sound.save path
     end
 
     def self.load(path)
-      self.new Beeps::Sound.load path
+      f = Beeps::FileIn.new path
+      self.new Beeps::Sound.new(f, f.seconds, nchannels: f.nchannels)
     end
 
   end# Sound
