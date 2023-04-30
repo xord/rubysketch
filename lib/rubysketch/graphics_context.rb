45b13d6beb3cf487c3bbaa812bc9f2506d0eb3a0
xordog <xordog@gmail.com>
2023-04-30 12:08:36 +0900

    add Sprite#on_draw__()

--- lib/rubysketch/graphics_context.rb
+++ lib/rubysketch/graphics_context.rb
@@ -11,28 +11,16 @@ module RubySketch
       sprites.flatten! if sprites.first&.is_a? Array
       sprites.each do |sp|
         v = sp.getInternal__
-        f, angle, img, offset = v.frame, v.angle, sp.image, sp.offset
+        f, angle = v.frame, v.angle
         if angle == 0
-          drawSprite__ f.x, f.y, f.w, f.h, img, offset
+          sp.on_draw__ f.x, f.y, f.w, f.h
         else
           pushMatrix do
             translate f.x, f.y
             rotate radians(angle)
-            drawSprite__ 0, 0, f.w, f.h, img, offset
+            sp.on_draw__ 0, 0, f.w, f.h
           end
         end
-      end
-    end
-
-    # @private
-    def drawSprite__(x, y, w, h, img, offset)
-      if img && offset
-        ox, oy = offset
-        copy img, ox, oy, w, h, x, y, w, h
-      elsif img
-        image img, x, y
-      else
-        rect x, y, w, h
       end
     end
 
