a361cabff36946e01fdcf2a9c168e54c96eb787f
xordog <xordog@gmail.com>
2023-05-08 01:37:56 +0900

    draw sprite with pivot rotation

--- lib/rubysketch/context.rb
+++ lib/rubysketch/context.rb
@@ -75,19 +75,21 @@ module RubySketch
       sprites.flatten! if sprites.first&.is_a? Array
       sprites.each do |sp|
         view, draw = sp.getInternal__, sp.instance_variable_get(:@drawBlock__)
-        f, degrees = view.frame, view.angle
+        f, degrees, pivot = view.frame, view.angle, view.pivot
         if draw
           push do
-            translate f.x, f.y
+            translate f.x + pivot.x * f.w, f.y + pivot.y * f.h
             rotate fromDegrees__ degrees
+            translate      -pivot.x * f.w,      -pivot.y * f.h
             draw.call {drawSprite__ sp, 0, 0, f.w, f.h}
           end
         elsif degrees == 0
           drawSprite__ sp, f.x, f.y, f.w, f.h
         else
           pushMatrix do
-            translate f.x, f.y
+            translate f.x + pivot.x * f.w, f.y + pivot.y * f.h
             rotate fromDegrees__ degrees
+            translate      -pivot.x * f.w,      -pivot.y * f.h
             drawSprite__ sp, 0, 0, f.w, f.h
           end
         end
