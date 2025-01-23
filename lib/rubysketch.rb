9e25020c9020ced514f8db1364ffccc940369f26
xordog <xordog@gmail.com>
2025-01-24 03:49:56 +0900

    Event blocks can be defined as methods with 'def'

--- lib/rubysketch.rb
+++ lib/rubysketch.rb
@@ -2,19 +2,26 @@ require 'rubysketch/all'
 
 
 module RubySketch
-  w = (ENV['WIDTH']  || 500).to_i
-  h = (ENV['HEIGHT'] || 500).to_i
+  w         = (ENV['WIDTH']  || 500).to_i
+  h         = (ENV['HEIGHT'] || 500).to_i
   WINDOW__  = Processing::Window.new(w, h) {start}
   CONTEXT__ = RubySketch::Context.new WINDOW__
+  BINDING__ = binding
 
   refine Object do
     (CONTEXT__.methods - Object.instance_methods)
       .reject {_1 =~ /__$/} # methods for internal use
       .each do |method|
-        define_method(method) do |*args, **kwargs, &block|
-          CONTEXT__.__send__(method, *args, **kwargs, &block)
+        define_method method do |*args, **kwargs, &block|
+          CONTEXT__.__send__ method, *args, **kwargs, &block
         end
       end
+
+    def Object.method_added(method)
+      if Processing::Context::EVENT_NAMES__.include? method
+        CONTEXT__.__send__(method) {BINDING__.eval method.to_s}
+      end
+    end
   end
 end# RubySketch
 
